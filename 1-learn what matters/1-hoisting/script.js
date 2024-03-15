4121026341afd1b500ef993532dd10f7b37b632c
frrukh <mhmdfrrukh13@gmail.com>
2024-03-15 22:40:07 +0500

    difference b/w var let and const

--- 1-learn what matters/1-hoisting/script.js
+++ 1-learn what matters/1-hoisting/script.js
@@ -1,6 +1,6 @@
 // HOISTING
 // Hoisting - the variables and functions (and classes) are hoisted i.e their declarations are moved toward top of the code.
-// simple -- every declaration is moved to the top of the file while compilation is called hoisting.
+// simple -- every declaration is moved to the top of the scope while compilation is called hoisting.
 
     // there are two things while creating a variable:
         // declaration : var a;
@@ -11,12 +11,12 @@
         // 1: var a;  -- here the value of a is undefined.
         // 1: a = 3;  -- here the value of a is updated to 3.
 
-    // after that the interpreter make the declaration part to the top of the file.so if we print a before declaring the variable using "var" the js interpreter prints undefined instead of showing any error like 'ReferenceError: Cannot access 'b' before initialization'
+    // after that the interpreter make the declaration part to the top of the scope.so if we print a before declaring the variable using "var" the js interpreter prints undefined instead of showing any error like 'ReferenceError: Cannot access 'b' before initialization'
 
     console.log(a)    // undefined
     var a = 3;
     
-    // this is same in functions.functions are shifted to the top of the file while compilation.
+    // this is same in functions.functions are shifted to the top of the scope while compilation.
 
 sum(1,2) // 3 
 function sum(a, b) {
@@ -35,7 +35,7 @@ var b = 3;
 
 // TWIST:
 // in case of let and const the hoisting works differently.
-    // in let and const the hoisting works like : it moves the variable to the top of the file but not declare the variable and also not initialize it.
+    // in let and const the hoisting works like : it moves the variable to the top of their scope but not declare the variable and also not initialize it.
         // so, take care of this.
     
 // console.log(d) // ReferenceError: Cannot access 'd' before initialization
@@ -43,3 +43,9 @@ let d = 4;
 
 // console.log(e) // ReferenceError: Cannot access 'e' before initialization
 let e = 3;
+
+
+// ********************
+// Q: if variables declared with let and const are not declared through hoisting then what is the advantage of hoisting for let and const?
+    // A: in case of let and const the hoisting only makes sure to check if the variable is available or not to help out the programmer to code more effectively.
+// ********************
